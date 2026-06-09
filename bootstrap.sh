634db25801ed6a208d8f1d86a80dd1d2e1c27d00
John Pierre <johndotpierre@outlook.com>
2026-06-09 14:27:36 -0500

    switching to zen browser

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 DOTFILES="$HOME/dotfiles"
 PACMAN_PACKAGES=(starship noto-fonts-emoji neovim onefetch stow wezterm fish lsd fastfetch keyd)
-AUR_PACKAGES=(noctalia-shell vesktop vicinae-bin maplemono-ttf maplemono-nf-unhinted maplemono-nf-cn-unhinted)
+AUR_PACKAGES=(noctalia-shell vesktop zen-browser-bin vicinae-bin maplemono-ttf maplemono-nf-unhinted maplemono-nf-cn-unhinted)
 
 #Quick update
 sudo pacman -Syu
