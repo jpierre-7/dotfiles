62fe19d30aea6a106ed73682d1d270f4d69ea6bd
John Pierre <johndotpierre@outlook.com>
2026-08-28 12:27:04 -0500

    Added tailscale to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 DOTFILES="$HOME/dotfiles"
-PACMAN_PACKAGES=(starship noctalia zed wlsunset vivaldi noto-fonts-emoji neovim onefetch stow wezterm fish lsd fastfetch keyd)
+PACMAN_PACKAGES=(tailscale starship noctalia zed wlsunset vivaldi noto-fonts-emoji neovim onefetch stow wezterm fish lsd fastfetch keyd)
 AUR_PACKAGES=(vesktop vicinae-bin maplemono-ttf maplemono-nf-unhinted maplemono-nf-cn-unhinted)
 
 #Quick update
